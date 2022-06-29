ccc2f73db35a991863b98b124ab92b72ec026deb
Budi Darmawan <vbudi@us.ibm.com>
2022-06-29 16:03:00 -0500

    pem using 2 databases
    Signed-off-by: Budi Darmawan <vbudi@us.ibm.com>

--- db2/createDB.sh
+++ db2/createDB.sh
@@ -1,4 +1,6 @@
 #!/bin/sh +
 
 whoami
-sudo -i -u db2inst1 bash -c '/opt/ibm/db2/V11.5/bin/db2 -stvf /var/custom/create_pemdb.sql -z create.log'
+
+sudo -i -u db2inst1 bash -c '/opt/ibm/db2/V11.5/bin/db2 -stvf /var/custom/create_pemdb.sql -z create_pemdb.log'
+sudo -i -u db2inst1 bash -c '/opt/ibm/db2/V11.5/bin/db2 -stvf /var/custom/create_pemtest.sql -z create_pemtest.log'
